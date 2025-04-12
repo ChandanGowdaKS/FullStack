5bd6c379cdaa54c910f78234335adbb9b831cb02
Chandan Gowda K S <chanduchess122@gmail.com>
2025-04-12 23:19:56 +0530

    Add files via upload

---
+++ Java Script/Datatypes,conditions/script.js
@@ -0,0 +1,21 @@
+// console.log(10);
+// console.log("Hello Karnataka");
+
+// var a = 10;
+// // var a;
+// console.log(a);
+
+// let b = 20;
+// console.log(b);
+// let c = "30";
+// console.log(a+b);
+// console.log(a-c);
+// console.log(typeof c);
+// console.log(typeof (a+c));
+
+let a = 10;
+let b = "10";
+
+console.log(a == b); // true it will check only value
+console.log(a != b); // false it will check only value
+console.log(a === b); // false it will data type and value
---
+++ Java Script/Questions/script.js
@@ -0,0 +1,36 @@
+// 1st Question
+
+console.log("hello word");
+
+// sum of two numbers
+
+let a = 10;
+let b = 20;
+console.log("The sum of two numbers is :", a+b);
+
+// even or odd
+
+if (a %2 == 0){
+    console.log("Its even");
+}
+else{
+    console.log("It's Odd");
+}
+
+// Calculator
+
+console.log("The addition of Two number is :",a+b);
+console.log("The Subtraction of Two number is :",a-b);
+console.log("The multiplication of Two number is :",a*b);
+console.log("The division of Two number is :",a/b);
+
+// Printing 1-100 numbers
+
+// let c = 1;
+// while (c<101){
+//     console.log(c);
+//     c++;
+// }
+
+// How to reverse a number
+
